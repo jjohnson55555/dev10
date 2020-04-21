581f87f754f5760fee2664fa64f5c20ad95c740d
jjohnson55555 <60401024+jjohnson55555@users.noreply.github.com>
2020-04-21 00:22:01 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
-var xhr = new XMLHttpRequest();       
+var xhr = new XMLHttpRequest();       // Create XMLHttpRequest object
 
 xhr.onload = function() {   
     var responseObject = JSON.parse(xhr.responseText);
 
-    
+    // BUILD UP STRING WITH NEW CONTENT (could also use DOM manipulation)
     var newContent = '';
     for (var i = 0; i < responseObject.events.length; i++) { 
       newContent += '<div class="event">';
@@ -15,9 +15,9 @@ xhr.onload = function() {
       newContent += '</div>';
     }
 
-    
+    // Update the page with the new content
     document.getElementById('content').innerHTML = newContent;
 };
 
-xhr.open('GET', 'data/data.json', true);        
-xhr.send(null);                                 
+xhr.open('GET', "data/data.json", true);        // Prepare the request
+xhr.send(null);                                 // Send the request
